a7bd111bd5eb39aac4958482983bf15f55a575e0
romanov-clockwise <serhii.romanov@clockwise.software>
2022-09-06 12:40:07 +0300

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div>It works</div>;
-}
+import { useEffect, useState } from 'react';
+
+const App = () => {
+	const [count, setCount] = useState([]);
+
+	useEffect(() => {
+		setCount([]);
+	}, [count]);
+
+	return <div>App</div>;
+};
 
 export default App;
